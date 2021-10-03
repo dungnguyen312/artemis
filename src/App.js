24ff0e400a0141f99bd107a53f30f3190fc62789
ThuyDung <dung.nguyen@paradox.ai>
2021-10-03 18:05:07 +0700

    Deploy & Prettier Config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import artemis from './static/artemis-goodness.jpg';
-import './App.css';
+import artemis from "./static/artemis-goodness.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={artemis} className="App-logo" alt="logo" />
-        <p>Welcome you to <code>Artemis</code>.
+        <p>
+          Welcome you to <code>Artemis</code>.
         </p>
       </header>
     </div>
