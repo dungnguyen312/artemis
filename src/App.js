cd031a77720b0e25c721b21a37ab2a388e1e7b7a
ThuyDung <dung.nguyen@paradox.ai>
2021-10-03 16:51:39 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import artemis from './static/artemis-goodness.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={artemis} className="App-logo" alt="logo" />
+        <p>Welcome you to <code>Artemis</code>.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
